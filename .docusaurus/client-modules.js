c97a5fb08a4fdd0afabd6b90dba1d0f001ecdef0
Destino <111420909+Destino233@users.noreply.github.com>
2023-12-02 22:48:13 +0800

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("P:\\888\\github\\doc\\DOC-NEW\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("P:\\888\\github\\doc\\DOC-NEW\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("P:\\888\\github\\doc\\DOC-NEW\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("P:\\888\\github\\doc\\DOC-NEW\\src\\css\\custom.css"),
-];
